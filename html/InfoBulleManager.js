bf54b5d87399b1a0199b545713f9c28e826867eb
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-10-01 14:56:27 +0200

    update

--- html/InfoBulleManager.js
+++ html/InfoBulleManager.js
@@ -15,10 +15,14 @@ export function addInfoBulle() {
 
     const infoBulleCount = selectedScene.tags.filter(tag => tag.type === 'infoBulle').length;
     const infoBulleName = `infoBulle${infoBulleCount + 1}`;
+    const infoBulleTitle = 'Sample Title';
+    const infoBulleDesc = 'Sample Description';
     var rotation = cameraEl.rotation.clone();
 
     selectedScene.tags.push({
         type: 'infoBulle',
+        title: infoBulleTitle,
+        desc: infoBulleDesc,
         position: { x: position.x, y: position.y, z: position.z },
         rotation: { x: 0, y: rotation.y, z: rotation.z },
         name: infoBulleName
@@ -36,20 +40,22 @@ export function addInfoBulle() {
     infoPanelEntity.setAttribute('id', `${infoBulleName}-info-panel`);
     infoPanelEntity.setAttribute('position', '0 1 -3');
     // infoPanelEntity.setAttribute('visible', 'false');
-
+    
     var infoPlane = document.createElement('a-plane');
     infoPlane.setAttribute('color', '#FFF');
     infoPlane.setAttribute('height', '1');
     infoPlane.setAttribute('width', '2');
-
+    
     var infoTextTitle = document.createElement('a-text');
-    infoTextTitle.setAttribute('value', 'Information Panel');
+    infoTextTitle.setAttribute('id', `${infoBulleName}-title`);
+    infoTextTitle.setAttribute('value', infoBulleTitle);
     infoTextTitle.setAttribute('color', '#000');
     infoTextTitle.setAttribute('position', '0.5 0 10');
     // infoTextTitle.setAttribute('opacity', '1');
-
+    
     var infoTextDescription = document.createElement('a-text');
-    infoTextDescription.setAttribute('value', 'This is a sphere.');
+    infoTextDescription.setAttribute('id', `${infoBulleName}-description`);
+    infoTextDescription.setAttribute('value', infoBulleDesc);
     infoTextDescription.setAttribute('color', '#000');
     infoTextDescription.setAttribute('position', '-0.8 -0.1 0');
 
@@ -75,15 +81,19 @@ function templatageInfoBulle(event) {
 
     const sceneSelect = document.getElementById('selectscene');
     const selectedScene = VR.scenes[sceneSelect.value];
-    console.log(event);
     const infBulle = selectedScene.tags.find(tag => tag.type === "infoBulle" && tag.name === event.target.id);
     console.log(infBulle);
 
     if (infBulle) {
         temp = temp.replaceAll("{{name}}", infBulle.name);
+        temp = temp.replaceAll("{{title}}", infBulle.title);
+        temp = temp.replaceAll("{{description}}", infBulle.desc);
         temp = temp.replaceAll("{{rangeValueX}}", infBulle.position.x);
         temp = temp.replaceAll("{{rangeValueY}}", infBulle.position.y);
         temp = temp.replaceAll("{{rangeValueZ}}", infBulle.position.z);
+        temp = temp.replaceAll("{{rangeValueRx}}", infBulle.rotation.x);
+        temp = temp.replaceAll("{{rangeValueRy}}", infBulle.rotation.y);
+        temp = temp.replaceAll("{{rangeValueRz}}", infBulle.rotation.z);
         recipe.innerHTML = temp;
         recipe.className = "fixed h-[97%] border-solid border-custom-blue z-10 bg-custom-white overflow-y-scroll px-6 py-0 rounded-lg right-2.5 top-2.5 border-2 border-custom-blue";
     }
@@ -100,7 +110,6 @@ export function loadInfoBulle() {
     }
 
     selectedScene.tags.forEach(tag => {
-        console.log(tag);
         if (tag.type === 'infoBulle') {
             var sphereEntity = document.createElement('a-sphere');
             sphereEntity.setAttribute('position', tag.position.x + ' ' + tag.position.y + ' ' + tag.position.z);
@@ -108,25 +117,30 @@ export function loadInfoBulle() {
             sphereEntity.setAttribute('color', '#EF2D5E');
             sphereEntity.setAttribute('class', 'link');
             sphereEntity.setAttribute('id', `${tag.name}-sphere`);
+            sphereEntity.addEventListener('click', function (event) {
+                switchAnimInfoBulle(event);
+            });
 
             var infoPanelEntity = document.createElement('a-entity');
             infoPanelEntity.setAttribute('id', `${tag.name}-info-panel`);
             infoPanelEntity.setAttribute('position', tag.position.x + ' ' + tag.position.y + ' ' + tag.position.z);
             infoPanelEntity.setAttribute('visible', 'false');
-
+            
             var infoPlane = document.createElement('a-plane');
             infoPlane.setAttribute('color', '#FFF');
             infoPlane.setAttribute('height', '1');
             infoPlane.setAttribute('width', '2');
-
+            
             var infoTextTitle = document.createElement('a-text');
-            infoTextTitle.setAttribute('value', 'Information Panel');
+            infoTextTitle.setAttribute('id', `${tag.name}-title`);
+            infoTextTitle.setAttribute('value', tag.title);
             infoTextTitle.setAttribute('color', '#000');
             infoTextTitle.setAttribute('position', (tag.position.x + 0.5) + ' ' + tag.position.y + ' ' + tag.position.z);
             infoTextTitle.setAttribute('opacity', '0');
-
+            
             var infoTextDescription = document.createElement('a-text');
-            infoTextDescription.setAttribute('value', 'This is a sphere.');
+            infoTextDescription.setAttribute('id', `${tag.name}-description`);
+            infoTextDescription.setAttribute('value', tag.desc);
             infoTextDescription.setAttribute('color', '#000');
             infoTextDescription.setAttribute('position', tag.position.x + ' ' + (tag.position.y - 1) + ' ' + (tag.position.z + 0.1)); // -0.8 -0.1 0
 
@@ -148,10 +162,10 @@ export function loadInfoBulle() {
 
 
 function switchAnimInfoBulle(ev) {
-    console.log(ev.target);
-    var panel = document.querySelector('#info-panel');
-    var text = document.querySelector('#info-text');
-    var sphere = document.querySelector('#sphere-test');
+    let baseId = ev.target.id.split('-')[0];
+
+    var panel = document.querySelector(`#${baseId}-info-panel`);
+    var sphere = document.querySelector(`#${baseId}-sphere`);
 
     var isVisible = panel.getAttribute('visible');
     panel.setAttribute('visible', !isVisible);
@@ -160,7 +174,7 @@ function switchAnimInfoBulle(ev) {
         sphere.setAttribute('animation', 'property: radius; to: 0.2; dur: 1000');
         sphere.setAttribute('animation__pos', 'property: position; to: 1 1.5 -3; dur: 1000');
     } else {
-        text.setAttribute('animation', 'property: opacity; to: 0; dur: 500');
+        // text.setAttribute('animation', 'property: opacity; to: 0; dur: 500');
         sphere.setAttribute('animation', 'property: radius; to: 0.5; dur: 1000');
         sphere.setAttribute('animation__pos', 'property: position; to: 0 1.25 -3; dur: 1000');
     }
@@ -176,11 +190,15 @@ export function ModifyInfoBulle(e) {
         inputRange.addEventListener('input', InfBullePositionChange);
     });
 
-    /* let SwitchInfoBulle = document.getElementById(`${e.target.id}-sphere`);
-    SwitchInfoBulle.addEventListener('click', function () {
-        switchAnimInfoBulle(e)
-        console.log('click');
-    }); */
+    let inputRangesRotation = document.querySelectorAll('.rotation')
+    inputRangesRotation.forEach(inputRange => {
+        inputRange.addEventListener('input', InfBulleRotationChange);
+    });
+
+    let rangeInputs = document.querySelectorAll('.inputRange');
+    rangeInputs.forEach(rgInput => {
+        LoadSlider(rgInput);
+    });
 }
 
 
@@ -206,8 +224,31 @@ export function InfBullePositionChange(e) {
             infBullePanel.setAttribute('position', newPosition);
         }
     }
-
-    console.log(VR.scenes);
     LoadSlider(e.target);
     loadInfoBulle();
+}
+
+export function InfBulleRotationChange(e) {
+    const infBulleName = document.getElementById('infbulle-name').textContent;
+    const sceneSelect = document.getElementById('selectscene');
+    const selectedScene = VR.scenes[sceneSelect.value];
+    const axis = e.target.name; // 'x', 'y', or 'z'
+    const infBullePosition = parseFloat(e.target.value);
+    let axisAlone = axis.slice(1);
+
+
+    document.querySelector(`#${axis}-value`).textContent = `${infBullePosition}`;
+
+    const infBulle = selectedScene.tags.find(tag => tag.type === 'infoBulle' && tag.name === infBulleName);
+    if (infBulle) {
+        infBulle.rotation = { ...infBulle.rotation, [axisAlone]: infBullePosition };
+
+        const infBulleElement = document.querySelector(`#info-bulle-entity #${infBulleName}`);
+        if (infBulleElement) {
+            infBulleElement.setAttribute('rotation', `${infBulle.rotation.x} ${infBulle.rotation.y} ${infBulle.rotation.z}`);
+        }
+    }
+    LoadSlider(e.target);
+    loadInfoBulle();
+    console.log(VR.scenes);
 }
