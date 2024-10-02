4696336b65939cd6d75123935258b7b7b005ce51
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-10-02 11:43:16 +0200

    up en cours Info Bulle toujours

--- html/InfoBulleManager.js
+++ html/InfoBulleManager.js
@@ -30,13 +30,16 @@ export function addInfoBulle() {
         name: infoBulleName
     });
 
+    var globalEntity = document.createElement('a-entity');
+    globalEntity.setAttribute('id', `${infoBulleName}-global-panel`);
+    globalEntity.setAttribute('position', position.x + ' ' + position.y + ' ' + position.z);
+    globalEntity.object3D.rotation.set(0, rotation.y, rotation.z);
+
     var sphereEntity = document.createElement('a-sphere');
-    sphereEntity.setAttribute('position', position.x + ' ' + position.y + ' ' + position.z);
     sphereEntity.setAttribute('radius', '0.5');
     sphereEntity.setAttribute('color', '#EF2D5E');
     sphereEntity.setAttribute('class', 'link');
     sphereEntity.setAttribute('id', `${infoBulleName}-sphere`);
-    sphereEntity.object3D.rotation.set(0, rotation.y, rotation.z);
 
     var infoPanelEntity = document.createElement('a-entity');
     infoPanelEntity.setAttribute('id', `${infoBulleName}-info-panel`);
@@ -52,22 +55,28 @@ export function addInfoBulle() {
     infoTextTitle.setAttribute('id', `${infoBulleName}-title`);
     infoTextTitle.setAttribute('value', infoBulleTitle);
     infoTextTitle.setAttribute('color', '#000');
-    infoTextTitle.setAttribute('position', position.x + 0.5 + ' ' + position.y + ' ' + position.z);
     infoTextTitle.setAttribute('opacity', '0');
+    infoTextTitle.setAttribute('width', '2');
+    infoTextTitle.setAttribute('wrap-count', '30');
 
     var infoTextDescription = document.createElement('a-text');
     infoTextDescription.setAttribute('id', `${infoBulleName}-description`);
     infoTextDescription.setAttribute('value', infoBulleDesc);
     infoTextDescription.setAttribute('color', '#000');
-    infoTextDescription.setAttribute('position', position.x + ' ' + position.y - 1 + ' ' + position.z + 0.1);
+    infoTextDescription.setAttribute('width', '2');
+    infoTextDescription.setAttribute('wrap-count', '30');
 
     infoPlane.appendChild(infoTextTitle);
     infoPlane.appendChild(infoTextDescription);
 
     infoPanelEntity.appendChild(infoPlane);
 
-    document.querySelector('#info-bulle-entity').appendChild(sphereEntity);
-    document.querySelector('#info-bulle-entity').appendChild(infoPanelEntity);
+    globalEntity.appendChild(sphereEntity);
+    globalEntity.appendChild(infoPanelEntity);
+
+    document.querySelector('#info-bulle-entity').appendChild(globalEntity);
+    // document.querySelector('#info-bulle-entity').appendChild(sphereEntity);
+    // document.querySelector('#info-bulle-entity').appendChild(infoPanelEntity);
     AddSceneExplorer(infoBulleName, 'infbulle');
 }
 
@@ -88,9 +97,7 @@ function templatageInfoBulle(event) {
         } else {
             valueVisible = '';
         }
-        console.log(infBulle);
-        console.log(valueVisible);
-        
+
         temp = temp.replaceAll("{{name}}", infBulle.name);
         temp = temp.replaceAll("{{title}}", infBulle.title);
         temp = temp.replaceAll("{{description}}", infBulle.desc);
@@ -118,8 +125,14 @@ export function loadInfoBulle() {
 
     selectedScene.tags.forEach(tag => {
         if (tag.type === 'infoBulle') {
+            var globalEntity = document.createElement('a-entity');
+            globalEntity.setAttribute('id', `${tag.name}-global-panel`);
+            globalEntity.setAttribute('position', tag.position.x + ' ' + tag.position.y + ' ' + tag.position.z);
+            globalEntity.object3D.rotation.set(0, tag.rotation.y, tag.rotation.z);
+
             var sphereEntity = document.createElement('a-sphere');
-            sphereEntity.setAttribute('position', tag.position.x + ' ' + tag.position.y + ' ' + tag.position.z);
+            console.log(tag);
+
             sphereEntity.setAttribute('radius', '0.5');
             sphereEntity.setAttribute('color', '#EF2D5E');
             sphereEntity.setAttribute('class', 'link');
@@ -127,7 +140,6 @@ export function loadInfoBulle() {
 
             var infoPanelEntity = document.createElement('a-entity');
             infoPanelEntity.setAttribute('id', `${tag.name}-info-panel`);
-            infoPanelEntity.setAttribute('position', tag.position.x + ' ' + tag.position.y + ' ' + tag.position.z);
             infoPanelEntity.setAttribute('visible', tag.isVisible);
 
             var infoPlane = document.createElement('a-plane');
@@ -139,21 +151,30 @@ export function loadInfoBulle() {
             infoTextTitle.setAttribute('id', `${tag.name}-title`);
             infoTextTitle.setAttribute('value', tag.title);
             infoTextTitle.setAttribute('color', '#000');
-            infoTextTitle.setAttribute('position', '-1 0.25 0.01');
+            infoTextTitle.setAttribute('position', '-0.95 0.25 0.01');
+            infoTextTitle.setAttribute('width', '2');
+            infoTextTitle.setAttribute('wrap-count', '30');
 
             var infoTextDescription = document.createElement('a-text');
             infoTextDescription.setAttribute('id', `${tag.name}-description`);
             infoTextDescription.setAttribute('value', tag.desc);
             infoTextDescription.setAttribute('color', '#000');
-            infoTextDescription.setAttribute('position', '-1 -0.25 0.01');
+            infoTextDescription.setAttribute('position', '-0.95 -0.25 0.01');
+            infoTextDescription.setAttribute('width', '2');
+            infoTextDescription.setAttribute('wrap-count', '30');
 
             infoPlane.appendChild(infoTextTitle);
             infoPlane.appendChild(infoTextDescription);
 
             infoPanelEntity.appendChild(infoPlane);
 
-            infoBulleEntities.appendChild(sphereEntity);
-            infoBulleEntities.appendChild(infoPanelEntity);
+            globalEntity.appendChild(sphereEntity);
+            globalEntity.appendChild(infoPanelEntity);
+
+            infoBulleEntities.appendChild(globalEntity);
+
+            // infoBulleEntities.appendChild(sphereEntity);
+            // infoBulleEntities.appendChild(infoPanelEntity);
 
             sphereEntity.addEventListener('click', function (event) {
                 switchAnimInfoBulle(event);
@@ -170,18 +191,23 @@ function switchAnimInfoBulle(ev) {
     var title = document.querySelector(`#${baseId}-title`);
     var desc = document.querySelector(`#${baseId}-description`);
 
+    console.dir(sphere);
+    
+
     var isVisible = panel.getAttribute('visible');
     panel.setAttribute('visible', !isVisible);
 
     const sceneSelect = document.getElementById('selectscene');
     const selectedScene = VR.scenes[sceneSelect.value];
     const infBulle = selectedScene.tags.find(tag => tag.type === "infoBulle" && tag.name === baseId);
+    console.log(infBulle);
     
+
     if (!isVisible) {
         title.setAttribute('animation', 'property: opacity; to: 1; dur: 500');
         desc.setAttribute('animation', 'property: opacity; to: 1; dur: 500');
         sphere.setAttribute('animation', 'property: radius; to: 0.2; dur: 1000');
-        sphere.setAttribute('animation__pos', `property: position; to: ${infBulle.position.x + 1} ${infBulle.position.y + 0.5} ${infBulle.position.z}; dur: 1000`); //1 1.5 -3
+        sphere.setAttribute('animation__pos', `property: position; to: ${infBulle.position.x + 0.5} ${infBulle.position.y + 0.5} ${infBulle.position.z}; dur: 1000`); //1 1.5 -3
     } else {
 
         title.setAttribute('animation', 'property: opacity; to: 0; dur: 500');
@@ -198,7 +224,12 @@ export function ModifyInfoBulle(e) {
 
     let CheckboxOpen = document.getElementById('checkboxOpen');
     CheckboxOpen.addEventListener('click', function () {
-        switchAnimInfoBulle(e);
+        InfBulleVisibleOrNot(e);
+    });
+
+    let ButtonName = document.getElementById('NameButton');
+    ButtonName.addEventListener('click', function () {
+        InfBulleTitle(e.target.id);
     });
 
     let inputRangesPosition = document.querySelectorAll('.position')
@@ -231,13 +262,11 @@ export function InfBullePositionChange(e) {
     if (infBulle) {
         infBulle.position = { ...infBulle.position, [axis]: infBullePosition };
 
-        const infBulleSphere = document.querySelector(`#info-bulle-entity #${infBulleName}-sphere`);
-        const infBullePanel = document.querySelector(`#info-bulle-entity #${infBulleName}-info-panel`);
+        const infBulleGlobal = document.querySelector(`#${infBulleName}-global-panel`);
 
-        if (infBulleSphere && infBullePanel) {
+        if (infBulleGlobal) {
             const newPosition = `${infBulle.position.x} ${infBulle.position.y} ${infBulle.position.z}`;
-            infBulleSphere.setAttribute('position', newPosition);
-            infBullePanel.setAttribute('position', newPosition);
+            infBulleGlobal.setAttribute('position', newPosition);
         }
     }
     LoadSlider(e.target);
@@ -259,12 +288,32 @@ export function InfBulleRotationChange(e) {
     if (infBulle) {
         infBulle.rotation = { ...infBulle.rotation, [axisAlone]: infBullePosition };
 
-        const infBulleElement = document.querySelector(`#info-bulle-entity #${infBulleName}`);
-        if (infBulleElement) {
-            infBulleElement.setAttribute('rotation', `${infBulle.rotation.x} ${infBulle.rotation.y} ${infBulle.rotation.z}`);
+        const infBulleGlobal = document.querySelector(`#${infBulleName}-global-panel`);
+        if (infBulleGlobal) {
+            infBulleGlobal.setAttribute('rotation', `${infBulle.rotation.x} ${infBulle.rotation.y} ${infBulle.rotation.z}`);
         }
     }
     LoadSlider(e.target);
+    loadInfoBulle();
+}
+
+
+export function InfBulleTitle(nom) {
+    let sceneName = document.getElementById('selectscene').value;
+    let scene = VR.scenes[sceneName];
+    let tags = scene.tags;
+    let tag = tags.find(isGoodInfBulle);
+
+    let valueInputTitle = document.getElementById('textInfoBulleTitle').value;
+    let valueInputDesc = document.getElementById('textInfoBulleDesc').value;
+
+    tag.title = valueInputTitle;
+    tag.desc = valueInputDesc;
+
+    function isGoodInfBulle(text) {
+        return text.name === nom;
+    }
+
     loadInfoBulle();
 }
 
@@ -276,6 +325,5 @@ function InfBulleVisibleOrNot(e) {
     let InputChecked = document.getElementById('checkboxOpen').checked;
 
     infBulle.isVisible = InputChecked;
-
     loadInfoBulle();
 }
