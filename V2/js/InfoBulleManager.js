cc0808d3cbe555d8c72fb600f313ffe4f38974c9
thechewgum <quentin.brandy@protonmail.com>
2024-10-03 23:00:49 +0200

    update 3D model

--- V2/js/InfoBulleManager.js
+++ V2/js/InfoBulleManager.js
@@ -215,6 +215,7 @@ export function switchAnimInfoBulle(ev) {
   let baseId = ev.target.id.split("-")[0];
   var panel = document.querySelector(`#${baseId}-info-panel`);
   var sphere = document.querySelector(`#${baseId}-sphere`);
+  var model = document.querySelector(`#${baseId}-model`);
   var title = document.querySelector(`#${baseId}-title`);
   var desc = document.querySelector(`#${baseId}-description`);
 
@@ -231,6 +232,11 @@ export function switchAnimInfoBulle(ev) {
   if (!isVisible) {
     title.setAttribute("animation", "property: opacity; to: 1; dur: 500");
     desc.setAttribute("animation", "property: opacity; to: 1; dur: 500");
+    model.setAttribute("animation", "property: radius; to: 0.2; dur: 1000");
+    model.setAttribute(
+      "animation__pos",
+      `property: position; to: 1 0.45 0; dur: 1000`
+    ); //1 1.5 -3
     sphere.setAttribute("animation", "property: radius; to: 0.2; dur: 1000");
     sphere.setAttribute(
       "animation__pos",
@@ -239,14 +245,22 @@ export function switchAnimInfoBulle(ev) {
   } else {
     title.setAttribute("animation", "property: opacity; to: 0; dur: 500");
     desc.setAttribute("animation", "property: opacity; to: 0; dur: 500");
-    sphere.setAttribute(
+    model.setAttribute(
       "animation",
       `property: radius; to: ${infBulle.radius}; dur: 1000`
     );
-    sphere.setAttribute(
+    model.setAttribute(
       "animation__pos",
       `property: position; to: 0 0 0; dur: 1000`
     ); //0 1.25 -3
+    sphere.setAttribute(
+        "animation",
+        `property: radius; to: ${infBulle.radius}; dur: 1000`
+      );
+      sphere.setAttribute(
+        "animation__pos",
+        `property: position; to: 0 0 0; dur: 1000`
+      ); //0 1.25 -3
   }
 }
 
--- V2/js/a-frame_entity.js
+++ V2/js/a-frame_entity.js
@@ -1,125 +1,142 @@
 import { TakeDoor } from "./DoorManager.js";
 import { switchAnimInfoBulle } from "./InfoBulleManager.js";
 
-function radToDeg(radians) {
-  return radians * (180 / Math.PI);
-}
 
 export function createEntity(tag) {
-  let newEntity;
+    let newEntity;
 
-  if (tag.type === "door") {
-    newEntity = document.createElement("a-sphere");
-    newEntity.setAttribute(
-      "position",
-      `${tag.position.x} ${tag.position.y} ${tag.position.z}`
-    );
-    newEntity.setAttribute("radius", "1");
-    newEntity.setAttribute("color", tag.fill);
-    newEntity.setAttribute("class", "link clickable");
-    newEntity.setAttribute(
-      "scale",
-      `${tag.scale.sx}  ${tag.scale.sy}  ${tag.scale.sz}`
-    );
-    newEntity.setAttribute("id", tag.name);
-    newEntity.addEventListener("click", function (event) {
-      TakeDoor(event);
-    });
-  } else if (tag.type === "text") {
-    newEntity = document.createElement("a-text");
-    newEntity.setAttribute(
-      "position",
-      `${tag.position.x} ${tag.position.y} ${tag.position.z}`
-    );
-    newEntity.setAttribute("value", tag.content);
-    newEntity.setAttribute("color", tag.fill);
-    newEntity.setAttribute("align", "center");
-    newEntity.setAttribute(
-      "scale",
-      `${tag.scale.sx}  ${tag.scale.sy}  ${tag.scale.sz}`
-    );
-    newEntity.setAttribute("id", tag.name);
-    newEntity.setAttribute("rotation", {
-      x: tag.rotation.rx,
-      y: tag.rotation.ry,
-      z: tag.rotation.rz,
-    });
-  } else if (tag.type === "photo") {
-    newEntity = document.createElement("a-image");
-    newEntity.setAttribute(
-      "position",
-      `${tag.position.x} ${tag.position.y} ${tag.position.z}`
-    );
-    newEntity.setAttribute("src", tag.src);
-    newEntity.setAttribute(
-      "scale",
-      `${tag.scale.sx}  ${tag.scale.sy}  ${tag.scale.sz}`
-    );
-    newEntity.setAttribute("id", tag.name);
-    newEntity.setAttribute("width", tag.taille.width);
-    newEntity.setAttribute("height", tag.taille.height);
-    newEntity.setAttribute("rotation", {
-      x: tag.rotation.rx,
-      y: tag.rotation.ry,
-      z: tag.rotation.rz,
-    });
-  } else if (tag.type === "infoBulle") {
-    newEntity = document.createElement("a-entity");
-    newEntity.setAttribute("id", `${tag.name}`);
-    newEntity.setAttribute(
-      "position",
-      tag.position.x + " " + tag.position.y + " " + tag.position.z
-    );
-    console.log(tag);
+    if (tag.type === "door") {
+        newEntity = document.createElement("a-sphere");
+        newEntity.setAttribute(
+            "position",
+            `${tag.position.x} ${tag.position.y} ${tag.position.z}`
+        );
+        newEntity.setAttribute("radius", "1");
+        newEntity.setAttribute("color", tag.fill);
+        newEntity.setAttribute("opacity", "0.5"); // Lower the opacity
+        newEntity.setAttribute("class", "link clickable");
+        newEntity.setAttribute(
+            "scale",
+            `${tag.scale.sx}  ${tag.scale.sy}  ${tag.scale.sz}`
+        );
+        newEntity.setAttribute("id", tag.name);
+        newEntity.addEventListener("click", function (event) {
+            TakeDoor(event);
+        });
 
-    newEntity.setAttribute("rotation", {
-      x: tag.rotation.rx,
-      y: tag.rotation.ry,
-      z: tag.rotation.rz,
-    });
+        // Add 3D object inside the sphere
+        var modelEntity = document.createElement("a-entity");
+        modelEntity.setAttribute("obj-model", "obj: url(../assets/3d/door/model.obj); mtl: url(../assets/3d/door/materials.mtl)");
+        modelEntity.setAttribute("animation", "property: rotation; to: 0 360 0; loop: true; dur: 10000; easing: linear");
+        modelEntity.setAttribute("scale", "2 2 2 "); // Adjust scale as needed
+        modelEntity.setAttribute("position", "0 0 0"); // Adjust position as needed
+        modelEntity.setAttribute("rotation", "0 0 0"); // Adjust rotation as needed
 
-    var sphereEntity = document.createElement("a-sphere");
-    sphereEntity.setAttribute("id", `${tag.name}-sphere`);
-    sphereEntity.setAttribute("radius", tag.radius);
-    sphereEntity.setAttribute("color", "#EF2D5E");
-    sphereEntity.setAttribute("class", "link clickable movableBox");
-    sphereEntity.addEventListener("click", function (event) {
-      switchAnimInfoBulle(event);
-    });
+        newEntity.appendChild(modelEntity);
+    } else if (tag.type === "text") {
+        newEntity = document.createElement("a-text");
+        newEntity.setAttribute(
+            "position",
+            `${tag.position.x} ${tag.position.y} ${tag.position.z}`
+        );
+        newEntity.setAttribute("value", tag.content);
+        newEntity.setAttribute("color", tag.fill);
+        newEntity.setAttribute("align", "center");
+        newEntity.setAttribute(
+            "scale",
+            `${tag.scale.sx}  ${tag.scale.sy}  ${tag.scale.sz}`
+        );
+        newEntity.setAttribute("id", tag.name);
+        newEntity.setAttribute("rotation", {
+            x: tag.rotation.rx,
+            y: tag.rotation.ry,
+            z: tag.rotation.rz,
+        });
+    } else if (tag.type === "photo") {
+        newEntity = document.createElement("a-image");
+        newEntity.setAttribute(
+            "position",
+            `${tag.position.x} ${tag.position.y} ${tag.position.z}`
+        );
+        newEntity.setAttribute("src", tag.src);
+        newEntity.setAttribute(
+            "scale",
+            `${tag.scale.sx}  ${tag.scale.sy}  ${tag.scale.sz}`
+        );
+        newEntity.setAttribute("id", tag.name);
+        newEntity.setAttribute("width", tag.taille.width);
+        newEntity.setAttribute("height", tag.taille.height);
+        newEntity.setAttribute("rotation", {
+            x: tag.rotation.rx,
+            y: tag.rotation.ry,
+            z: tag.rotation.rz,
+        });
+    } else if (tag.type === "infoBulle") {
+        newEntity = document.createElement("a-entity");
+        newEntity.setAttribute("id", `${tag.name}`);
+        newEntity.setAttribute(
+            "position",
+            tag.position.x + " " + tag.position.y + " " + tag.position.z
+        );
+        console.log(tag);
 
-    var infoPanelEntity = document.createElement("a-entity");
-    infoPanelEntity.setAttribute("id", `${tag.name}-info-panel`);
-    infoPanelEntity.setAttribute("visible", tag.isVisible);
+        newEntity.setAttribute("rotation", {
+            x: tag.rotation.rx,
+            y: tag.rotation.ry,
+            z: tag.rotation.rz,
+        });
 
-    var infoPlane = document.createElement("a-plane");
-    infoPlane.setAttribute("color", "#FFF");
-    infoPlane.setAttribute("width", "2");
-    infoPlane.setAttribute("height", "1");
+        var sphereEntity = document.createElement("a-sphere");
+        sphereEntity.setAttribute("id", `${tag.name}-sphere`);
+        sphereEntity.setAttribute("radius", tag.radius);
+        sphereEntity.setAttribute("color", "#EF2D5E");
+        sphereEntity.setAttribute("opacity", "0.1");
+        sphereEntity.setAttribute("class", "link clickable movableBox");
+        sphereEntity.addEventListener("click", function (event) {
+            switchAnimInfoBulle(event);
+        });
 
-    var infoTextTitle = document.createElement("a-text");
-    infoTextTitle.setAttribute("id", `${tag.name}-title`);
-    infoTextTitle.setAttribute("value", tag.title);
-    infoTextTitle.setAttribute("position", "-0.95 0.25 0.01");
-    infoTextTitle.setAttribute("color", tag.titleColor);
-    infoTextTitle.setAttribute("opacity", "0");
-    infoTextTitle.setAttribute("width", "1.9");
-    infoTextTitle.setAttribute("wrap-count", "30");
+        var modelEntity = document.createElement("a-entity");
+        modelEntity.setAttribute("id", `${tag.name}-model`);
+        modelEntity.setAttribute("obj-model", "obj: url(../assets/3d/info/model.obj); mtl: url(../assets/3d/info/materials.mtl)");
+        modelEntity.setAttribute("scale", "1 1 1"); // Adjust scale as needed
+        modelEntity.setAttribute("position", "0 0 0"); // Adjust position as needed
+        modelEntity.setAttribute("rotation", "0 -90 0"); // Adjust rotation as needed
+        modelEntity.setAttribute("animation", "property: rotation; to: 0 360 0; loop: true; dur: 10000; easing: linear"); // Add animation
+        var infoPanelEntity = document.createElement("a-entity");
+        infoPanelEntity.setAttribute("id", `${tag.name}-info-panel`);
+        infoPanelEntity.setAttribute("visible", tag.isVisible);
 
-    var infoTextDescription = document.createElement("a-text");
-    infoTextDescription.setAttribute("id", `${tag.name}-description`);
-    infoTextDescription.setAttribute("value", tag.desc);
-    infoTextDescription.setAttribute("position", "-0.95 -0.25 0.01");
-    infoTextDescription.setAttribute("color", tag.descColor);
-    infoTextDescription.setAttribute("width", "1.9");
-    infoTextDescription.setAttribute("wrap-count", "30");
+        var infoPlane = document.createElement("a-plane");
+        infoPlane.setAttribute("color", "#FFF");
+        infoPlane.setAttribute("width", "2");
+        infoPlane.setAttribute("height", "1");
 
-    infoPlane.appendChild(infoTextTitle);
-    infoPlane.appendChild(infoTextDescription);
+        var infoTextTitle = document.createElement("a-text");
+        infoTextTitle.setAttribute("id", `${tag.name}-title`);
+        infoTextTitle.setAttribute("value", tag.title);
+        infoTextTitle.setAttribute("position", "-0.95 0.25 0.01");
+        infoTextTitle.setAttribute("color", tag.titleColor);
+        infoTextTitle.setAttribute("opacity", "0");
+        infoTextTitle.setAttribute("width", "1.9");
+        infoTextTitle.setAttribute("wrap-count", "30");
 
-    infoPanelEntity.appendChild(infoPlane);
+        var infoTextDescription = document.createElement("a-text");
+        infoTextDescription.setAttribute("id", `${tag.name}-description`);
+        infoTextDescription.setAttribute("value", tag.desc);
+        infoTextDescription.setAttribute("position", "-0.95 -0.25 0.01");
+        infoTextDescription.setAttribute("color", tag.descColor);
+        infoTextDescription.setAttribute("width", "1.9");
+        infoTextDescription.setAttribute("wrap-count", "30");
 
-    newEntity.appendChild(sphereEntity);
-    newEntity.appendChild(infoPanelEntity);
-  }
-  return newEntity;
+        infoPlane.appendChild(infoTextTitle);
+        infoPlane.appendChild(infoTextDescription);
+
+        infoPanelEntity.appendChild(infoPlane);
+
+        newEntity.appendChild(sphereEntity);
+        newEntity.appendChild(modelEntity);
+        newEntity.appendChild(infoPanelEntity);
+    }
+    return newEntity;
 }
