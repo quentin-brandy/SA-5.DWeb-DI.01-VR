078e8215e3b740a69ea66401ee4df6bfd2f94f22
thechewgum <quentin.brandy@protonmail.com>
2024-11-04 15:15:18 +0100

    update js

--- VFinal/js/Tagclass.js
+++ VFinal/js/Tagclass.js
@@ -3,112 +3,7 @@ export class TagManager {
     this.scene = scene;
     this.tags = scene.tags;
   }
-
-  addTag(type, name, position, rotation = {}, additionalProperties = {}) {
-    const tag = {
-      type,
-      name,
-      position,
-      rotation,
-      ...additionalProperties,
-    };
-    this.scene.tags.push(tag);
-    return tag; // Retourne le tag pour des manipulations supplémentaires
-  }
-
-  moveTag(name, newPosition) {
-    const tag = this.tags.find((t) => t.name === name);
-    if (tag) {
-      tag.position = { ...newPosition };
-      return tag;
-    }
-    return null;
-  }
-
-  rotateTag(name, newRotation) {
-    const tag = this.tags.find((t) => t.name === name);
-    if (tag) {
-      tag.rotation = { ...newRotation };
-      return tag;
-    }
-    return null;
-  }
-  scaleTag(name, newScale) {
-    const tag = this.getTag(name);
-    if (tag) {
-      tag.scale = newScale;
-      return tag;
-    }
-    return null;
-  }
-  updateTagSize(name, newSize, dimension) {
-    const tag = this.getTag(name);
-    if (
-      tag &&
-      tag.taille &&
-      (dimension === "width" || dimension === "height")
-    ) {
-      tag.taille[dimension] = newSize;
-      return tag;
-    }
-    return null;
-  }
-  getTag(name) {
-    return this.tags.find((t) => t.name === name);
-  }
-
-  renameTag(oldName, newName) {
-    const tag = this.getTag(oldName);
-    if (tag) {
-      tag.name = newName;
-    }
-  }
-
-  updateTagFill(name, fill) {
-    const tag = this.getTag(name);
-    if (tag) {
-      tag.fill = fill;
-      return tag;
-    }
-  }
-  deleteTag(name) {
-    const index = this.tags.findIndex((t) => t.name === name);
-    if (index !== -1) {
-      this.tags.splice(index, 1);
-      return true;
-    }
-    return false;
-  }
-
-  duplicateTag(tagName) {
-    const originalTag = this.getTag(tagName);
-    if (originalTag) {
-      const newTagName = this.generateUniqueName(
-        originalTag.name,
-        originalTag.type
-      );
-      const newTag = {
-        ...originalTag,
-        name: newTagName,
-        position: { ...originalTag.position },
-      };
-      this.scene.tags.push(newTag);
-      return newTag;
-    }
-    return null;
-  }
-
-  generateUniqueName(baseName, type) {
-    let newName = baseName;
-    let count = 1;
-    while (this.tags.some((tag) => tag.name === newName)) {
-      newName = `${baseName}_copy${count}`;
-      count++;
-    }
-    return newName;
-  }
 }
-
 export class Door extends TagManager {
   addDoorTag(
     name,
@@ -160,15 +55,6 @@ export class Photo extends TagManager {
       taille,
       scale,
     });
-  }
-
-  updatePhotoSrc(name, newSrc) {
-    const tag = this.getTag(name);
-    if (tag && tag.type === "photo") {
-      tag.src = newSrc;
-      return tag;
-    }
-    return null;
   }
 }
 
--- VFinal/js/main.js
+++ VFinal/js/main.js
@@ -1,135 +1,41 @@
 
-
-// Function to save VR object to localStorage
-function saveVRToLocalStorage() {
-  localStorage.setItem("VR", JSON.stringify(VR));
-}
-
-// Function to load VR object from localStorage
-function loadVRFromLocalStorage() {
-  const savedVR = localStorage.getItem("VR");
-  if (savedVR && savedVR !== "{}") {
-    VR = JSON.parse(savedVR);
-    console.log("Loaded VR from localStorage:", VR);
-  } else {
-    VR = {
-      scenes: {
-        scene1: {
-          name: "scene1",
-          tags: [],
-          image: {
-            url: "../assets/img/1.jpg",
-            name: "1.jpg",
-          },
-        },
-          scene2: {
-            name: "scene2",
-            tags: [],
-            image: {
-              url: "../assets/img/2.jpg",
-              name: "1.jpg",
-            },
-        },
-        defaultScene: "scene1",
-      },
-    };
-    console.log("Initialized default VR:", VR);
-  }
-}
-
-// Declare VR variable
-let VR;
-
-// Call loadVRFromLocalStorage when the page loads
-loadVRFromLocalStorage();
-
-// Set an interval to call saveVRToLocalStorage every 120 seconds (120000 milliseconds)
-setInterval(saveVRToLocalStorage, 120000);
-
-export default VR;
-
-import { addDoor } from "./DoorManager.js";
-import {
-  AddScene,
-  DeleteScene,
-  ChangeSceneName,
-  DuplicateScene,
-  AddSceneSelectOption,
-  switchScene,
-  setDefaultScene,
-  checkAndSpawnDefaultScene,
-} from "./SceneManager.js";
-import { addText } from "./TextManager.js";
-import { addPhoto } from "./PhotoManager.js";
-import { addInfoBulle } from "./InfoBulleManager.js";
-
-const actions = {
-  "plus-scene": AddScene,
-  "minus-scene": DeleteScene,
-  switchscenename: ChangeSceneName,
-  "duplicate-scene": DuplicateScene,
-  "plus-door": addDoor,
-  "plus-text": addText,
-  "plus-photo": addPhoto,
-  "plus-infoBulle": addInfoBulle,
-  "export-button": saveVRToZip,
-  "delete-save": ResetAll,
-  "save-button": saveVRToLocalStorage,
-  "default-scene-checkbox": setDefaultScene,
+// Declare VR variable with initial scenes structure
+let VR = {
+  scenes: {},
+  defaultScene: null
 };
 
-Object.keys(actions).forEach((id) => {
-  let element = document.getElementById(id);
-  if (element) {
-    element.addEventListener("click", actions[id]);
+// Function to load VR object from JSON file
+async function loadVRFromJSON() {
+  try {
+    const response = await fetch('../assets/json/VR.json');
+    const data = await response.json();
+    VR = data;
+    console.log("Loaded VR from JSON:", VR);
+  } catch (error) {
+    console.log("Using default VR structure:", VR);
   }
-});
-
-document.getElementById("selectscene").addEventListener("change", switchScene);
-
-async function saveVRToZip() {
-  let zip = new JSZip();
-  let vrFolder = zip.folder("VR");
-  vrFolder.file("VR.json", JSON.stringify(VR));
-
-  // Add assets folder to the zip
-  let assetsFolder = vrFolder.folder("assets");
-  let imgFolder = assetsFolder.folder("img");
-
-  // Assuming you have a list of image URLs to add to the zip
-  let imageUrls = []; // Add more image URLs as needed
-  // Collect image URLs from VR object
-  for (let sceneKey in VR.scenes) {
-    if (VR.scenes.hasOwnProperty(sceneKey) && VR.scenes[sceneKey].image) {
-      imageUrls.push(VR.scenes[sceneKey].image.url);
-    }
-  }
-  for (let url of imageUrls) {
-    let response = await fetch(url);
-    let blob = await response.blob();
-    let fileName = url.split("/").pop();
-    imgFolder.file(fileName, blob);
-  }
-
-  zip.generateAsync({ type: "blob" }).then(function (content) {
-    let a = document.createElement("a");
-    a.href = URL.createObjectURL(content);
-    a.download = "VR.zip";
-    a.click();
-  });
+  return VR;
 }
 
-function ResetAll() {
-  localStorage.clear();
-  location.reload();
+// Initialize VR
+async function initVR() {
+  const loadedVR = await loadVRFromJSON();
+  return loadedVR;
 }
 
-let rightController = document.getElementById("rightController");
-rightController.addEventListener("abuttondown", () => {
-  let selectedScene = document.querySelector("sceneselect");
-  selectedScene.value = VR.scenes.defaultScene;
-  switchScene();
-});
+// Initialize VR
+const initializedVR = await initVR();
+VR = initializedVR;
+console.log("Initialized VR:", VR);
+export default VR;
+
+import {
+  AddSceneSelectOption,
+  checkAndSpawnDefaultScene,
+} from "./SceneManager.js";
+
+
 
 AddSceneSelectOption();
 checkAndSpawnDefaultScene();
